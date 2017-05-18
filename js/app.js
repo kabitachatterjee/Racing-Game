dfc28e9c9a679cc255d32495f925da66a57a0f62
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-05-17 18:29:28 -0700

    added winning logic to the game

--- js/app.js
+++ js/app.js
@@ -1,25 +1,22 @@
 console.log("JS file linked");
 
-var width = $(window).width();
+let width = $(window).width();
 console.log(width);
-var countOfClicks1 = 0;
-var countOfClicks2 = 0;
+let countOfClicks1 = 0;
+let countOfClicks2 = 0;
  winner = localStorage;
- console.log(winner.getItem('Mickey') );
- console.log(winner.getItem('Donald') );
 
 
 $(window).keypress(function (e) {
-  //countOfClicks++;
 
-        if (e.keyCode === 32 && (width - (165 + countOfClicks1 * 150) >=280)) {
+        if (e.keyCode === 109 && (width - (165 + countOfClicks1 * 150) >=280)) {
           countOfClicks1++;
           $("#hero1").css("margin-left","+=150px");
           winner.setItem('Mickey', countOfClicks1);
 
       }
 
-  else if (e.keyCode === 122 && (width - (165 + countOfClicks2 * 150) >=280)) {
+  else if (e.keyCode === 100 && (width - (165 + countOfClicks2 * 150) >=280)) {
         countOfClicks2++;
         $("#hero2").css("margin-left","+=150px");
         winner.setItem('Donald', countOfClicks2);
@@ -27,32 +24,25 @@ $(window).keypress(function (e) {
 
     else {
       location.reload();
-
-
-
+      $(document).ready(function(){
+        setWinner();
+});
 }
 
 
 
 });
+function setWinner(){
+  console.log(winner);
+  if( winner.getItem('Mickey') > winner.getItem('Donald') ){
 
-// function reloadCode() {
-//     if (window.location.hash.substr(1) == "triggerReloadCode") {
-//         window.location.hash = "";
-//         /* Statements */
-//         console.log("hello");
-//         console.log(winner.getItem('Mickey') );
-//         console.log(winner.getItem('Donald') );
-//         if( winner.getItem('Mickey') > winner.getItem('Donald') ){
-//
-//           console.log("Mickey won!!");
-//         }
-//         else if( winner.getItem('Donald') > winner.getItem('Mickey') ){
-//           console.log("Donald won!!");
-//         }
-//         else {
-//           console.log("It's a tie!!");
-//         }
-//
-//     }
-// }
+            console.log("Mickey won!!");
+          }
+          else if( winner.getItem('Donald') > winner.getItem('Mickey') ){
+            console.log("Donald won!!");
+          }
+          else {
+            console.log("It's a tie!!");
+          }
+
+}
